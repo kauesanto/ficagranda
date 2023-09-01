2ec1f6271c5a53593867149b91e098341b088616
kauesanto <129408833+kauesanto@users.noreply.github.com>
2023-09-01 09:35:01 -0300

    js

---
+++ js/principal.js
@@ -0,0 +1,11 @@
+ //atribui o valor da tag h1 par a variável titulo 
+ let titulo = document.querySelector("h1").textContent;
+ //mostra no console do navegador a variável título
+ console.log(titulo);
+  //modifica o valor do titulo para a frase "So veneno kkkkkk"
+  document.querySelector("h1").textContent = "So veneno kkkkk"
+
+  //-----------aqui vamos mexer na tabela de imc--------------
+
+  let paciente = document.querySelector("#primeiro-paciente")
+ console.log(paciente)
