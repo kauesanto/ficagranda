7da113e39f725931f75c30caca5694b7d0129cd5
kauesanto <129408833+kauesanto@users.noreply.github.com>
2023-09-15 10:48:15 -0300

    FICA

--- js/principal.js
+++ js/principal.js
@@ -6,41 +6,49 @@
   document.querySelector("h1").textContent = "So veneno kkkkk";
 
   //-----------aqui vamos mexer na tabela de imc--------------
-  // armazena na variavel as paciente as informações do primeiro paciente 
-  let paciente = document.querySelector("#primeiro-paciente");
- console.log(paciente);
-  //armazena somente as colunas de peso e altura 
-  let tdPeso = paciente.querySelector(".info-peso");
-  let tdAltura = paciente.querySelector(".info-altura");
-  //armazena somente as informações de texto de peso e altura 
-  let peso = tdPeso.textContent;
-  let altura = tdAltura.textContent;
+  // ----------armazena no array  as paciente as informações de todos os paciente 
+  let pacientes = document.querySelectorAll(".paciente");
+ //---------console.log(paciente);
+ //--------console.log(paciente);
+//----------loop for para percorrer o array 
+for(let i =0; i < pacientes.length; i++){
+  let paciente = pacientes[i];
+  console.log(paciente);
+//-----------armazena somente as colunas de peso e altura
+  
+let tdPeso = paciente.querySelector(".info-peso");
+let tdAltura = paciente.querySelector(".info-altura");
+//armazena somente as informações de texto de peso e altura 
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
 
-//acessa a coluna imc e altera o valor para o resutado 
+//-----------acessa a coluna imc e altera o valor para o resutado 
 let tdImc = paciente.querySelector(".info-imc");
 
 
-//validação dos dados 
+//----------------validação dos dados -----------
 let pesoEhValido = true;
 let alturaEhValida = true;
 
 if(peso < 0 || peso > 1000){
-  console.log("peso inválido!");
- tdImc.textContent = "Peso invalido";
-  pesoEhValido = false;
+console.log("peso inválido!");
+tdImc.textContent = "Peso invalido";
+pesoEhValido = false;
 } 
 if(altura < 0 || altura > 3){
-    console.log("altura inválida!");
-   tdImc.textContent = "altura invalida";
-   alturaEhValida = false;
+  console.log("altura inválida!");
+ tdImc.textContent = "altura invalida";
+ alturaEhValida = false;
 }   
 
 if(pesoEhValido === true && alturaEhValida === true){
- //calcula o imc 
-  let imc = peso/(altura * altura);
-  console.log (imc);
-  tdImc.textContent = imc;
+//calcula o imc 
+let imc = peso/(altura * altura);
+console.log (imc);
+tdImc.textContent = imc.toFixed(2);
 }else{
- tdImc.textContent = "peso e/ou altura inválidos!";
+  //iforma que os valore informados para peso e/outro
+tdImc.textContent = "peso e/ou altura inválidos!";
+}
 }
 
